cfcf0fe1a47459e485f2e334dfa2877f623dbf9c
Benjamin Bermont <benji-du-51@hotmail.fr>
2017-11-03 01:07:40 +0100

    Refactoring et ajout de commentaires

--- app/src/main/java/com/lille1/bermont/appelautreappfinal/MainActivity.java
+++ app/src/main/java/com/lille1/bermont/appelautreappfinal/MainActivity.java
@@ -10,8 +10,6 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -27,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQ_CODE_SPEECH_INPUT = 100;
     private TextView mVoiceInputTv;
     private ImageButton mSpeakBtn;
+    private String nomAi = "David AI : ";
+    // Numéro de téléphone d'exemple pour les émulateurs qui n'ont pas de contact dans le répertoire
     private String phoneNumber = "0612345678";
 
     @Override
@@ -37,22 +37,31 @@ public class MainActivity extends AppCompatActivity {
         mVoiceInputTv = (TextView) findViewById(R.id.micro_indication);
         mSpeakBtn = (ImageButton) findViewById(R.id.btnSpeak);
         mSpeakBtn.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
                 startVoiceInput();
             }
         });
 
-        // exemple pour itinéraire Google Maps
+        // Affiche un itinéraire sur une carte (Google Maps)
         Button btn_itineraire = (Button) findViewById(R.id.btn_itineraire);
         btn_itineraire.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                doItineraire();
+                doAfficherItineraire();
             }
         });
 
+        // Prank : Attendre 2030 ou 2035 pour les robots domestiques
+        Button btn_coffee = (Button) findViewById(R.id.btn_coffee);
+        btn_coffee.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doCafe();
+            }
+        });
+
+        // Appel le numéro de téléphone en mémoire ou celui sélectionné
         Button btn_appeler = (Button) findViewById(R.id.btn_appeler);
         btn_appeler.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Envoie un sms au numéro de téléphone en mémoire ou celui sélectionné
         Button btn_sms = (Button) findViewById(R.id.btn_sms);
         btn_sms.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,67 +79,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Partage un message parmi une liste d'application (boîte de dialogue de sélection)
         Button btn_chooser = (Button) findViewById(R.id.btn_chooser);
         btn_chooser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                appChooser();
+                doMontrerAppChooser();
             }
         });
 
+        // Change l'activité en cours vers une nouvelle activité de notre application (Intent Explicite)
         Button btn_explicit_activity = (Button) findViewById(R.id.btn_explicit_activity);
         btn_explicit_activity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intentExplicit();
+                doChangerActivite();
             }
         });
 
-
+        // Permet de choisir un contact parmi son répertoire téléphonique
         Button btn_contact = (Button) findViewById(R.id.btn_getContact);
         btn_contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                pickContact();
+                doChoisirContact();
             }
         });
-
     }
 
-    private void pickContact() {
+    private void doCafe() {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(nomAi+"Tu prends un ou deux sucres ?");
+    }
+
+    private void doChoisirContact() {
         Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
-        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Montrer à l'utilisateur uniquement les contacts avec des numéros de téléphone
         startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Vérifie la requête à laquelle on répond (ici choisir contact)
-        if (requestCode == PICK_CONTACT_REQUEST) {
-            // Vérifie que la requête a fonctionné
-            if (resultCode == RESULT_OK) {
-                // Récupère l'URI qui pointe sur le contact sélectionné
-                Uri contactUri = data.getData();
-                // On a seulement besoin du numéro de la colonne, car il n'y aura qu'une ligne de résultat
-                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
-
-                // Effectue une requête sur un contact pour récupérer la colonne du numéro
-                // Pas besoin de trier, on ne peut recevoir qu'un seul numéro sur l'URI
-                Cursor cursor = getContentResolver()
-                        .query(contactUri, projection, null, null, null);
-                cursor.moveToFirst();
-
-                // Retrieve the phone number from the NUMBER column
-                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                String number = cursor.getString(column);
-                // stocke le numéro dans la variable privé phoneNumber de la classe
-                phoneNumber = number;
-
-                TextView textView = (TextView) findViewById(R.id.textView);
-                textView.setText("Que voulez-vous faire de ce numéro ? \n" + number);
-            }
-        }
-
         // Vérifie la requête à laquelle on répond (ici textToSpeech)
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
@@ -138,17 +128,46 @@ public class MainActivity extends AppCompatActivity {
                     mVoiceInputTv.setText(result.get(0));
 
                     // Déclenchement des actions selon ce qu'on dit à Dadid dans le micro
+                    if ((result).contains("faire le café") || (result).contains("café")) { doCafe(); }
+                    if ((result).contains("partager un message") || (result).contains("partager")) { doMontrerAppChooser(); }
                     if ((result).contains("appeler")) { doAppeler(); }
                     if ((result).contains("sms") || (result).contains("envoyer sms") || (result).contains("envoyer un sms")) { doEnvoyerSms(); }
-                    if ((result).contains("changer activité") || (result).contains("changer") || (result).contains("activité")) { intentExplicit(); }
-                    if ((result).contains("récupérer contact") || (result).contains("récupérer") || (result).contains("contact") || (result).contains("récupérer un contact")) { pickContact(); }
-                    if ((result).contains("afficher itinéraire") || (result).contains("afficher") || (result).contains("itinéraire")) { doItineraire(); }
+                    if ((result).contains("changer activité") || (result).contains("changer") || (result).contains("activité")) { doChangerActivite(); }
+                    if ((result).contains("récupérer contact") || (result).contains("récupérer") || (result).contains("contact") || (result).contains("récupérer un contact")) { doChoisirContact(); }
+                    if ((result).contains("afficher itinéraire") || (result).contains("afficher") || (result).contains("itinéraire")) { doAfficherItineraire(); }
+                }
+                break;
+            }
+            case PICK_CONTACT_REQUEST:{
+                // Vérifie que la requête a fonctionné
+                if (resultCode == RESULT_OK) {
+                    // Récupère l'URI qui pointe sur le contact sélectionné
+                    Uri contactUri = data.getData();
+                    // On a seulement besoin du numéro de la colonne, car il n'y aura qu'une ligne de résultat
+                    String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                    // Effectue une requête sur un contact pour récupérer la colonne du numéro
+                    // Pas besoin de trier, on ne peut recevoir qu'un seul numéro sur l'URI
+                    Cursor cursor = getContentResolver()
+                            .query(contactUri, projection, null, null, null);
+                    cursor.moveToFirst();
+
+                    // Récupère le numéro de téléphone depuis la colone des numéros
+                    int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                    String number = cursor.getString(column);
+                    // stocke le numéro dans la variable privé phoneNumber de la classe
+                    phoneNumber = number;
+
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(nomAi+"Que voulez-vous faire de ce numéro ? \n" + number);
                 }
                 break;
             }
         }
     }
 
+    // Démarrer la reconnaissance vocale
+    // On démarre un intent implicite qui va chercher une activité qui fait partie des fonctionnalités de base Android
     private void startVoiceInput() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
@@ -161,7 +180,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void doItineraire() {
+    private void doAfficherItineraire() {
         // Affichage d'un itinéraire de Paris à Lille
         Uri location = Uri.parse("http://maps.google.com/maps?saddr=Paris&daddr=Lille");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
@@ -178,7 +197,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doAppeler() {
-
         Uri number = Uri.parse("tel:"+phoneNumber);
         Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
 
@@ -202,7 +220,7 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void appChooser() {
+    private void doMontrerAppChooser() {
         Intent chooserIntent = new Intent(Intent.ACTION_SEND);
         chooserIntent.setType("text/plain");
         chooserIntent.putExtra(Intent.EXTRA_SUBJECT, "Super message");
@@ -213,11 +231,11 @@ public class MainActivity extends AppCompatActivity {
         String title = getResources().getString(R.string.msg_a_envoyer);
         // Création d'un intent pour visualiser le chooser
         Intent chooser = Intent.createChooser(chooserIntent, title);
-
         startActivity(chooser);
     }
 
-    private void intentExplicit() {
+    // Intent explicite : on appelle une activité de son application par le nom exacte.
+    private void doChangerActivite() {
         Intent intent = new Intent(this, ExplicitActivity.class);
         startActivity(intent);
     }
